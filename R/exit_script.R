f19808a7507d64eb3b5927c0eb514ab3cdf0b7f2
Jimmy O'Donnell <pearljim3@hotmail.com>
2015-10-23 10:33:40 -0700

    added example of how to exit scripts

---
+++ R/exit_script.R
@@ -0,0 +1,14 @@
+#!/usr/bin/env Rscript
+
+animal <- "cat"
+
+if(animal == "cat" ){
+   # warning("this is the warning message")
+   stop("this is the stop message")
+   # quit()
+} else {
+   print("it's not a cat!")
+}
+
+print("we're out of the loop!")
+
