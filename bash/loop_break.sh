540225302b681c975144c6c147aa52784115bef4
Jimmy O'Donnell <jodonnellbio@gmail.com>
2015-11-16 10:21:45 -0800

    added info on loop control

--- bash/loop_break.sh
+++ bash/loop_break.sh
@@ -1,16 +1,26 @@
 #!/usr/bin/env bash
 
+max_num=7
+min_num=4
 
-echo hello
+echo "hello, I am going to stop running once I get to a number greater than" "${max_num}"
+echo "for numbers less than" "${min_num}" "I will exit just that iteration of the loop, but not abort the rest of the loop, or exit the script."
+
 
 for i in {1..10}
 do
-  echo "this is $i"
-  echo "stopping at 7"
-  if [ $i -gt 7 ]; then
-    echo "hey, that was greater than 7 -- stopping the loop"
+  echo "this is" "${i}"
+  if [ $i -gt "${max_num}" ]; then
+    echo "hey, $i is greater than ${max_num} -- stopping the loop"
     break
   fi
+
+  if [ $i -lt "${min_num}" ]; then
+    echo "$i is less than" "${min_num}"", skipping to next loop iteration..."
+    continue
+  fi
+
+  echo "this is the statement at the end of the loop!"
 
 done
 
