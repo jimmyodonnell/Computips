50db50744899aabc43e361b399458df105e8e6a0
Jimmy O'Donnell <pearljim3@hotmail.com>
2016-02-09 00:20:53 -0800

    add trick to reference python key from value queries

---
+++ python/dictionary_key_from_value.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+# get key from python dictionary if value matches query
+[k for k, v in d.iteritems() if v == desired_value]
+
+# source: http://stackoverflow.com/questions/7657457/finding-key-from-value-in-python-dictionary
+# I guess this is called list comprehension?
