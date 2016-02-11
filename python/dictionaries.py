1da19a11d30a45c517dc57625a81fc55b6faf62c
Jimmy O'Donnell <pearljim3@hotmail.com>
2016-02-11 13:09:43 -0800

    add reference on referencing dictionaries

---
+++ python/dictionaries.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+# some stuff about dictionaries
+
+# If you only need the dictionary keys
+my_dict.keys().
+
+# If you only need the dictionary values
+my_dict.values().
+
+# If you want both, 
+my_dict.items()
+# will return a list of couples [(key1, value1), (key2, value2), ...].
+
+# if you want index numbers of 
+enumerate(my_dict)
