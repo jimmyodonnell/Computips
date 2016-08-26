ff13e1a3daca64d5ebba0bb8a1215392e5c7306e
Jimmy O'Donnell <jodonnellbio@gmail.com>
2016-08-25 18:44:29 -0700

    add alternative function

--- R/load_R_files.R
+++ R/load_R_files.R
@@ -5,3 +5,11 @@
 R_files <- list.files(path = "/path/to/directory", pattern = "*.R$", full.names = TRUE)
 sapply(R_files, source)
 
+# A function from the help files:
+sourceDir <- function(path, trace = TRUE, ...) {
+    for (nm in list.files(path, pattern = "\\.[RrSsQq]$")) {
+       if(trace) cat(nm,":")           
+       source(file.path(path, nm), ...)
+       if(trace) cat("\n")
+    }
+}
