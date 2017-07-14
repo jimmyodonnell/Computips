f1d6ccb9b23c920ebb4d07a43a2183a4552b6886
Jimmy O'Donnell <jodonnellbio@gmail.com>
2017-07-13 17:30:25 -0700

    small tweak

--- git/git_rm_dir.sh
+++ git/git_rm_dir.sh
@@ -10,6 +10,7 @@ for remote in `git branch -r | grep -v /HEAD`; do git checkout --track $remote ;
 # Remove DIRECTORY_NAME from all commits, then remove the refs to the old commits
 # (repeat these two commands for as many directories that you want to remove)
 git filter-branch --index-filter 'git rm -rf --cached --ignore-unmatch DIRECTORY_NAME/' --prune-empty --tag-name-filter cat -- --all
+
 git for-each-ref --format="%(refname)" refs/original/ | xargs -n 1 git update-ref -d
 
 # Ensure all old refs are fully removed
