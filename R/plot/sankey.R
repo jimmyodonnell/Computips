0826b810761d1d52dc021e2523e740a22c6afc05
Jimmy O'Donnell <jodonnellbio@gmail.com>
2017-12-11 12:56:59 -0800

    add sankey diagram

---
+++ R/plot/sankey.R
@@ -0,0 +1,11 @@
+library(networkD3)
+
+URL <- paste0(
+  "https://cdn.rawgit.com/christophergandrud/networkD3/",
+  "master/JSONdata/energy.json")
+Energy <- jsonlite::fromJSON(URL)
+
+sankeyNetwork(Links = Energy$links, Nodes = Energy$nodes, Source = "source",
+              Target = "target", Value = "value", NodeID = "name",
+              units = "TWh", fontSize = 12, nodeWidth = 30)
+
